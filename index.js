5f91fe10d5f0af90b14686ffea5ec43e398be75e
Ronald Avendaño <32528023+ravendano014@users.noreply.github.com>
2022-12-12 19:14:09 -0600

    Server 0.0.0.0

--- index.js
+++ index.js
@@ -2047,7 +2047,7 @@ app.get('*', (req, res) => {
 // 401 No autorizado
 // 405 Metodo no permitido
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), '0.0.0.0', () => {
   console.log('App:', app.get('appName'));
   console.log(`Server on port ${app.get('port')}`);
 });
