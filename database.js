c1bb4235f6184c0a7dab8120bee38c0c3a6ca0ef
Ronald Avendaño <32528023+ravendano014@users.noreply.github.com>
2022-12-14 09:57:40 -0600

    Pool Connect

--- database.js
+++ database.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql');
 
-var mysqlConnection = mysql.createConnection({  
+var mysqlConnection =  mysql.createPool({
+  connectionLimit : 10,
   host     : process.env.host_db,  
   user     : process.env.user_db,  
   password : process.env.password_db,  
   database : process.env.name_db,    
 });    
 
-mysqlConnection.connect(function (err) {
+pool.getConnection(function(err, connection) {
   if (err) {
     console.error(err);
     return;
